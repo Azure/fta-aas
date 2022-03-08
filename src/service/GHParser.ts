f7f2d15a219535e44aea6733d440b51640775c33
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2022-03-08 21:42:30 +0000

    ow joy it works!

--- src/service/GHParser.ts
+++ src/service/GHParser.ts
@@ -1,9 +1,31 @@
-import { IGHFileList } from "../model/IGHFileList";
+import { IGHFileList, ITreeItem } from "../model/IGHFileList";
 import { IParseResult } from "../model/IParseResult";
 
 class GHParser {
     parse(listToParse : IGHFileList) : IParseResult {
-        return {} as IParseResult;
+        var result : IParseResult = {checklists: []};
+        for ( var element of listToParse.tree){
+            var name = this.findTechnology(element.path);
+            var language = this.findLanguage(element.path);
+            var existing = result.checklists.find(o => o.name === name)
+            if (! existing) {
+                result.checklists.push({name: name, languages: [language], categories: []});
+            }
+            else {
+                existing.languages.push(language);
+            }
+        }
+        return result;
+    }
+    findLanguage(path: string) {
+        var startIndex = path.indexOf('.');
+        var endIndex = path.indexOf('.', startIndex);
+        return path.slice(startIndex, endIndex-startIndex);
+    }
+
+    findTechnology(element : string) : string {
+        var index = element.indexOf("_");
+        return element.slice(0, index);
     }
 }
 
--- src/tests/GHParser.test.ts
+++ src/tests/GHParser.test.ts
@@ -1,17 +1,57 @@
 import { IGHFileList, ITreeItem } from '../model/IGHFileList';
-import GHParser from '../service/GHParser';
+import GHParserInstance from '../service/GHParser';
 
-it('Finds the checklist name', () => {
-    // var parser = new GHParser();
-    // var item1 : ITreeItem = { path: "aks_checklist.en.json", url: new URL("http://nothing.com")};
-    // var item2 : ITreeItem = { path: "aks_checklist.es.json", url: new URL("http://nothing.com")};
-    // var item3 : ITreeItem = { path: "aks_checklist.ja.json", url: new URL("http://nothing.com")};
-    // var item4 : ITreeItem = { path: "aks_checklist.ko.json", url: new URL("http://nothing.com")};
-    // var listToParse : IGHFileList = {tree: [item1, item2, item3, item4]};
-    // var promiseOfListToParse : Promise<IGHFileList> = new Promise(() => { return listToParse; });
+it('Adds the checklist only once to the result', () => {
+    var item1 : ITreeItem = { path: "aks_checklist.en.json", url: new URL("http://nothing.com")};
+    var item2 : ITreeItem = { path: "aks_checklist.es.json", url: new URL("http://nothing.com")};
+    var item3 : ITreeItem = { path: "aks_checklist.ja.json", url: new URL("http://nothing.com")};
+    var item4 : ITreeItem = { path: "aks_checklist.ko.json", url: new URL("http://nothing.com")};
+    var listToParse : IGHFileList = {tree: [item1, item2, item3, item4]};
     
-    // return parser.parse(promiseOfListToParse)
-    //     .then(result => { 
-    //         expect(result.checklists).toContain("aks");
-    //     });
+    var result = GHParserInstance.parse(listToParse)
+    expect(result.checklists.length).toBe(1);
+});
+
+it('Adds another checklist if it sees another name', () => {
+    var item1 : ITreeItem = { path: "aks_checklist.en.json", url: new URL("http://nothing.com")};
+    var item2 : ITreeItem = { path: "aks_checklist.es.json", url: new URL("http://nothing.com")};
+    var item3 : ITreeItem = { path: "aks_checklist.ja.json", url: new URL("http://nothing.com")};
+    var item4 : ITreeItem = { path: "avd_checklist.ko.json", url: new URL("http://nothing.com")};
+    var listToParse : IGHFileList = {tree: [item1, item2, item3, item4]};
+    
+    var result = GHParserInstance.parse(listToParse)
+    expect(result.checklists.length).toBe(2);
+});
+
+it('Creates an aks checklist with all languages', () => {
+    var item1 : ITreeItem = { path: "aks_checklist.en.json", url: new URL("http://nothing.com")};
+    var item2 : ITreeItem = { path: "aks_checklist.es.json", url: new URL("http://nothing.com")};
+    var item3 : ITreeItem = { path: "aks_checklist.ja.json", url: new URL("http://nothing.com")};
+    var item4 : ITreeItem = { path: "aks_checklist.ko.json", url: new URL("http://nothing.com")};
+    var listToParse : IGHFileList = {tree: [item1, item2, item3, item4]};
+    
+    var result = GHParserInstance.parse(listToParse);
+    var aksChecklist = result.checklists.find(cl => cl.name === "aks");
+    expect(aksChecklist?.languages.length).toBe(4);
+});
+
+it('Can do this for multiple tech and languages', () => {
+    var item1 : ITreeItem = { path: "aks_checklist.en.json", url: new URL("http://nothing.com")};
+    var item2 : ITreeItem = { path: "avd_checklist.es.json", url: new URL("http://nothing.com")};
+    var item3 : ITreeItem = { path: "abc_checklist.ja.json", url: new URL("http://nothing.com")};
+    var item4 : ITreeItem = { path: "aks_checklist.ko.json", url: new URL("http://nothing.com")};
+    var item5 : ITreeItem = { path: "abc_checklist.ko.json", url: new URL("http://nothing.com")};
+    var item6 : ITreeItem = { path: "avd_checklist.ko.json", url: new URL("http://nothing.com")};
+    var item7 : ITreeItem = { path: "xyz_checklist.ko.json", url: new URL("http://nothing.com")};
+    var listToParse : IGHFileList = {tree: [item1, item2, item3, item4, item5, item6, item7]};
+    
+    var result = GHParserInstance.parse(listToParse);
+    var aksChecklist = result.checklists.find(cl => cl.name === "aks");
+    var avdChecklist = result.checklists.find(cl => cl.name === "avd");
+    var abcChecklist = result.checklists.find(cl => cl.name === "abc");
+    var xyzChecklist = result.checklists.find(cl => cl.name === "xyz");
+    expect(aksChecklist?.languages.length).toBe(2);
+    expect(avdChecklist?.languages.length).toBe(2);
+    expect(abcChecklist?.languages.length).toBe(2);
+    expect(xyzChecklist?.languages.length).toBe(1);
 });

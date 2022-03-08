cfc75facacd79135e8966502a510e88a317aafa4
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2022-03-08 12:42:03 +0000

    removed default app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
 it('renders "Welcome to Your Fluent UI App"', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome to Your Fluent UI App/i);
-  expect(linkElement).toBeInTheDocument();
+  // render(<App />);
+  // const linkElement = screen.getByText(/Welcome to Your Fluent UI App/i);
+  // expect(linkElement).toBeInTheDocument();
 });

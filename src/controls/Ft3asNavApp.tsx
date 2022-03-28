cef7a8dad1db222f4e9ece9660f06129580349c6
Felip Miguel <fmiguel@microsoft.com>
2022-03-29 00:57:45 +0200

    fix navigation without backend #75

--- src/controls/Ft3asNavApp.tsx
+++ src/controls/Ft3asNavApp.tsx
@@ -1,5 +1,5 @@
 import { INavLink, INavLinkGroup, Nav, Stack } from "@fluentui/react";
-import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch, useHistory } from "react-router-dom";
 import TelemetryProvider from "../service/telemetry-provider";
 import { getAppInsights } from "../service/TelemetryService";
 import Ft3asApp from "./Ft3asApp";
@@ -10,13 +10,14 @@ import Ft3asHome from "./Ft3asHome";
 export default function Ft3asNavApp() {
     const appInsightKey = process.env.REACT_APP_APP_INSIGHTS_KEY
     let appInsights = null;
-    const history= useHistory();
+    const history = useHistory();
 
     const _onLinkClick = (event?: React.MouseEvent<HTMLElement>, item?: INavLink) => {
-        if (item){
+        if (item) {
             history.push(item.url);
-            console.log('navigate to ' + item.url);
-            event?.stopPropagation();            
+            return false;
+            // console.log('navigate to ' + item.url);
+            // event?.stopPropagation();            
         }
     }
 
@@ -32,7 +33,7 @@ export default function Ft3asNavApp() {
                 },
                 {
                     name: 'Azure Design Review',
-                    url: '/#/checklist',
+                    url: 'checklist',
                     icon: 'CheckList',
                     key: 'key1',
 
@@ -42,8 +43,8 @@ export default function Ft3asNavApp() {
     ];
 
     return (
-        
-            <BrowserRouter>
+
+        <BrowserRouter>
             <TelemetryProvider instrumentationKey={appInsightKey} after={() => { appInsights = getAppInsights() }}>
                 <Stack horizontal>
                     <Stack.Item>
@@ -52,16 +53,16 @@ export default function Ft3asNavApp() {
                             selectedKey="key3"
                             ariaLabel="FTA as a Service"
                             groups={navLinkGroups}
+                            onRenderLink={(link) => link ? (<Link to={link.url}>{link.name}</Link>) : <></>}
                         />
                     </Stack.Item>
                     <Stack.Item>
                         <Switch>
                             <Route path="/checklist" component={Ft3asApp} />
-                            <Route path="/#/checklist" component={Ft3asApp} />
                             <Route path="/" component={Ft3asHome} />
                         </Switch>
                     </Stack.Item>
                 </Stack>
-                </TelemetryProvider>
-            </BrowserRouter >);
+            </TelemetryProvider>
+        </BrowserRouter >);
 }

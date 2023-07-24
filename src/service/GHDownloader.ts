6cc03fbdfd210dc4af4a441f2cf0ca7bcb786345
João Carlos Santos <jomacedo@microsoft.com>
2023-07-24 18:10:10 +0100

    Update GHDownloader to always use latest list of checklists.

--- src/service/GHDownloader.ts
+++ src/service/GHDownloader.ts
@@ -2,9 +2,7 @@ import { IGHFileList } from "../model/IGHFileList";
 
 class GHDownloader {
     getAllFiles(): Promise<IGHFileList> {
-        //TODO: The 'number' at the end is icky --> see if we can get this some other way
-        //found this through: https://api.github.com/repos/Azure/review-checklists/git/trees/main?recursive=1
-        return fetch("https://api.github.com/repos/Azure/review-checklists/git/trees/3f64f26773c9043a637b966758ddb7e45243da53")
+        return fetch("https://api.github.com/repos/Azure/review-checklists/git/trees/main?recursive=1")
             .then(response => response.json());
     }
 }

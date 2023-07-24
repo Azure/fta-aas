24cb36417a64ee485ba3429e73bf00a7f411c63f
João Carlos Santos <jomacedo@microsoft.com>
2023-07-24 18:15:13 +0100

    Now pointing to correct tree.

--- src/service/GHDownloader.ts
+++ src/service/GHDownloader.ts
@@ -2,7 +2,9 @@ import { IGHFileList } from "../model/IGHFileList";
 
 class GHDownloader {
     getAllFiles(): Promise<IGHFileList> {
-        return fetch("https://api.github.com/repos/Azure/review-checklists/git/trees/main?recursive=1")
+        //TODO: The 'number' at the end is icky --> see if we can get this some other way
+        //found this through: https://api.github.com/repos/Azure/review-checklists/git/trees/main?recursive=1
+        return fetch("https://api.github.com/repos/Azure/review-checklists/git/trees/4c75ff708221dc908139512d531bbc8c1d15c5b1")
             .then(response => response.json());
     }
 }

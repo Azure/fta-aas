9adeeca93f3e7d4ae1956a131f24df7b61908ca5
João Carlos Santos <jomacedo@microsoft.com>
2024-03-15 10:49:06 +0000

    Now fetching latest checklists

--- src/service/GHDownloader.ts
+++ src/service/GHDownloader.ts
@@ -1,14 +1,17 @@
 import { IGHFileList } from "../model/IGHFileList";
 
 class GHDownloader {
-    getAllFiles(): Promise<IGHFileList> {
+    async getAllFiles(): Promise<IGHFileList> {
         //TODO: The 'number' at the end is icky --> see if we can get this some other way
         //found this through: https://api.github.com/repos/Azure/review-checklists/git/trees/main?recursive=1
-        return fetch("https://api.github.com/repos/Azure/review-checklists/git/trees/a068a9c66b559e668393306849bef9661d82f0fa")
+        //"path": "checklists"
+        const response = await fetch('https://api.github.com/repos/Azure/review-checklists/commits?recursive=1');
+        const data = await response.json();
+        const gitTreeId = data[0]?.commit?.tree?.sha;
+        return fetch("https://api.github.com/repos/Azure/review-checklists/git/trees/"+gitTreeId+"?recursive=1")
             .then(response => response.json());
     }
 }
-
 const GHDownloaderInstance = new GHDownloader();
 
 export default GHDownloaderInstance;
